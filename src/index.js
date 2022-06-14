784713477b124074f7c185977be7ee7cf5980879
Brian Wangila <bwanjalah@gmail.com>
2022-06-14 14:47:31 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    doToList(e.target.new_task_description.value)
+  })
+
+
+  function doToList (todo){
+    let li = document.createElement("li")
+    li.textContent = todo
+    document.querySelector("#tasks").appendChild(li)
+  }
+
+
+
+
+
+
+
+  
 });
