c5d995367ad9964d34f03ad1249716ae39b08008
Brian Wangila <bwanjalah@gmail.com>
2022-06-14 15:21:40 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault()
     doToList(e.target.new_task_description.value)
+    form.reset()
   })
 
 
   function doToList (todo){
     let li = document.createElement("li")
-    li.textContent = todo
+    let btn = document.createElement("button")
+    btn.addEventListener("click", deleteList)
+
+    btn.textContent = "x"
+    li.textContent = `${todo} `
+    li.appendChild(btn)
     document.querySelector("#tasks").appendChild(li)
+
+    //styling the li
+    li.style.paddingBottom = "5px"
+    li.style.marginLeft = "-20px"
+    li.style.color = "brown"
+
+    btn.style.backgroundColor = "brown"
+    btn.style.fontWeight = "600"
+    btn.style.fontSize = "10px"
+    btn.style.height = "20px"
   }
 
-
-
-
+  function deleteList(e){
+    e.target.parentNode.remove()
+  }
 
 
 
